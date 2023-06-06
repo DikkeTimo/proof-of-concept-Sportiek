e6ddc67701f4f93f301a53956b5442a22c5fea94
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-06-06 11:39:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import express from "express";
 // Create a new Express app
 const app = express();
 const port = process.env.PORT || 4242;
+// php file from sportiek
+const sportiek = "https://www.sportiek.com/feed/wintersport2.php";
 
 // Set EJS as the template engine and specify the views directory
 app.set("view engine", "ejs");
@@ -16,9 +18,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async function (request, response) {
-  response.render("index");
+  fetchJson(sportiek).then((data) => {
+    console.log(data);
+    response.render("index", data);
+  });
 });
 
 app.listen(port, () => {
   console.log("listening on http://localhost:" + port);
 });
+
+async function fetchJson(url) {
+  return await fetch(sportiek)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
