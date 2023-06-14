f239d6ba71208cb221a704452dfd32e4631f9e36
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-06-14 09:40:06 +0200

    gitignore

--- indexx.js
+++ indexx.js
@@ -1,4 +1,8 @@
 import express from "express";
+import { config } from "dotenv";
+config();
+
+const apiKey = process.env.API_URL;
 
 // Create a new Express app
 const app = express();
@@ -6,18 +10,15 @@ const port = process.env.PORT || 4242;
 
 const sportiek =
   "https://raw.githubusercontent.com/DikkeTimo/proof-of-concept-Sportiek/main/json/localjssportiek.json";
-const sportiekone = "https://www.sportiek.com/feed/wintersport1.php";
+const sportiekone = [apiKey];
 
 const datasportiek = [[sportiek], [sportiekone]];
 const [data1, data2] = await Promise.all(datasportiek.map(fetchJson));
 const data = { data1, data2 };
 
 const checkdata = data1.filter((item) => {
-  return (item.departurePricePersons === 8 || item.departurePricePersons === 12)
+  return item.departurePricePersons === 8 || item.departurePricePersons === 12;
 });
-
-
-
 
 var accommodations = {};
 // Loop door alle data, en maak per accommodatie een nieuw object aan
@@ -48,11 +49,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async function (request, response) {
-  response.render("index", { data1: data1, checkdata: checkdata});
+  response.render("index", { data1: data1, checkdata: checkdata });
 });
 
 app.get("/overview", async function (request, response) {
-  response.render("overviewpage", { data2: data2, checkdata: checkdata});
+  response.render("overviewpage", { data2: data2, checkdata: checkdata });
 });
 
 app.listen(port, () => {
@@ -60,7 +61,19 @@ app.listen(port, () => {
 });
 
 async function fetchJson(urls) {
-  return await fetch(urls)
-    .then((response) => response.json())
-    .catch((error) => error);
+  // Display the loader
+  console.log("Loading...");
+
+  try {
+    const response = await fetch(urls);
+    const data = await response.json();
+    // Hide the loader once the response is received
+    console.log("Loaded!");
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    // Hide the loader in case of an error
+    console.log("Error!");
+    throw error;
+  }
 }
