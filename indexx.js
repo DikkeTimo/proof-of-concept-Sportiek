ab5c805e1baa0c1abcdeb33ae2a0cc0087400871
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-06-14 19:58:11 +0200

    filter option working

--- indexx.js
+++ indexx.js
@@ -16,27 +16,30 @@ const datasportiek = [[sportiek], [sportiekone]];
 const [data1, data2] = await Promise.all(datasportiek.map(fetchJson));
 const data = { data1, data2 };
 
-const checkdata = data1.filter((item) => {
-  return item.departurePricePersons === 8 || item.departurePricePersons === 12 || item.departurePricePersons === 6
-});
+// const checkdata = data1.filter((item) => {
+//   return item.numberOfBeds === item.departurePricePersons
+// });
 
-var accommodations = {};
-// Loop door alle data, en maak per accommodatie een nieuw object aan
-// if accommodations.id {
-// accommodations.id.push
-// else
-// accommodations.id = []
-// accommodations.id.push
-// accommodations: {
+const filterData = data1.reduce((acc, item) => {
+  const existingItem = acc.find((el) => el.variantName === item.variantName);
 
-// 975: [
-//   { ...},
-//   { ... },
-//   { ... }
-// ],
-// 877: [
+  if (existingItem) {
+    // Voeg de huidige datum alleen toe als deze nog niet in de bestaande item is opgenomen
+    if (!existingItem.departureDates.includes(item.departureDate)) {
+      existingItem.departureDates.push(item.departureDate);
+    }
+  } else {
+    // Voeg een nieuw item toe met de huidige variantName en datum
+    acc.push({
+      accomodationId: item.accomodationId,
+      variantName: item.variantName,
+      complex_name: item.complex_name,
+      departureDates: [item.departureDate]
+    });
+  }
 
-// ]}
+  return acc;
+}, []);
 
 // Set EJS as the template engine and specify the views directory
 app.set("view engine", "ejs");
@@ -49,7 +52,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async function (request, response) {
-  response.render("index", { data1: data1, checkdata: checkdata });
+  response.render("index", { data1: data1, filterData: filterData });
 });
 
 app.get("/overview", async function (request, response) {
@@ -60,10 +63,9 @@ app.listen(port, () => {
   console.log("listening on http://localhost:" + port);
 });
 
+
 async function fetchJson(urls) {
   // Display the loader
-  console.log("Loading...");
-
   try {
     const response = await fetch(urls);
     const data = await response.json();
